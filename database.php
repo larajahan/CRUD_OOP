32dfd9fc029ca798ed55cbd8f8e6c9a15458612c
shahjahanwp <shahjahanparamanik1@gmail.com>
2019-02-22 23:41:40 +0600

    Second Commit

---
+++ database.php
@@ -0,0 +1,21 @@
+<?php
+class Database{
+
+public $host = DB_HOST;
+public $user = DB_USER;
+public $pass = DB_PASS;
+public $name = DB_NAME;
+
+public $link;
+public $error;
+
+private function connectDB()
+{
+	$this->link = new mysqli($this->host,$this->user,$this->pass,$this->name);
+
+	if (!$this->link) {
+		$this->error ="Connection failed".$this->link->connect_error;
+	}
+}
+}
+?>
