1daad3ef564f12b0ec4e68ba08c49f9df3652f86
shahjahanwp <shahjahanparamanik1@gmail.com>
2019-02-23 20:32:18 +0600

    3rd commit

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
 <?php include 'inc/header.php'; ?>
+
+
+ <table class="tblone">
+
+ 	<tr> 		
+ 		<th width="25%">Name</th>
+ 		<th width="25%">Email</th>
+ 		<th width="25%">Skill</th>
+ 		<th width="25%">Actions</th>
+ 	</tr>
+
+ 	<tr>
+ 		<td>Name</td>
+ 		<td>Email</td>
+ 		<td>Skill</td>
+ 		<td href="update.php">Edit</td>
+
+ 	</tr>
+ 	
+
+
+ </table>
+
+
  
 <?php include 'inc/footer.php'; ?>
